541ed76a6808438ee9a10a9aaa7a85e57de154c0
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-05 18:53:00 +0200

    add props to component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,54 @@ import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 
-export default function App(){
+export default function App() {
   return (
     <>
-    <Nav />
-    <Hero />
-    <Card />
+      <Nav />
+      <Hero />
+      <div className="cards-container">
+        <Card
+          img="./src/assets/card-image.png"
+          rating="5.0"
+          reviewCount="6"
+          Country="USA"
+          title="Life lessons with Katie Zaferes"
+          price="136"
+        />
+        <Card
+          img="./src/assets/bike.jpg"
+          rating="6.1"
+          reviewCount="9"
+          Country="Rwanda"
+          title="Group Mountain Biking"
+          price="150"
+        />
+        <Card
+          img="./src/assets/teacher.png"
+          rating="4.5"
+          reviewCount="8"
+          Country="Spain"
+          title="Learn with teacher mwenyewe"
+          price="120"
+        />
+        <Card
+          img="./src/assets/flower.jpg"
+          rating="5.1"
+          reviewCount="5"
+          Country="Italian"
+          title="Learn wedding photography
+          "
+          price="200"
+        />
+        <Card
+          img="./src/assets/students.png"
+          rating="3.8"
+          reviewCount="3"
+          Country="TZ"
+          title="Learn academics within class"
+          price="100"
+        />
+      </div>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
-export default function Card(){
+export default function Card({img, rating, reviews, country, title, price}){
     return (
         <div className="card">
-            <img src="./src/assets/card-image.png" alt="car-image" className="card-image"/>
+            <img src={img} alt="car-image" className="card-image"/>
             <div className="card-details">
                 <img src="./src/assets/star.png" alt="star" className="card-star" />
-                <span>5.0</span>
-                <span className="gray">(6)</span>
-                <span className="gray">-USA</span>
+                <span>{rating}</span>
+                <span className="gray">({reviews})</span>
+                <span className="gray">-{country}</span>
             </div>
-            <p>Life lessons with kate zeran</p>
-            <p><span className="bold">from $136</span>/person</p>
+            <p>{title}</p>
+            <p><span className="bold">from ${price}</span>/person</p>
         </div>
     )
 }
