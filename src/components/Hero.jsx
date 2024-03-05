201409e26e0976f181ebf5752fe6771a1155a300
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-05 13:09:31 +0200

    add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      <img className="grid-image" src="./src/assets/grid-image.png" alt="" />
+      <div className="experience">
+        <h2>Online Experience</h2>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+}
