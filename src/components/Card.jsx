f4f4678888bca98909f8c9b05c7168b22b8cd0b1
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-05 21:17:16 +0200

    add component props with external data and conditional rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,33 @@
-export default function Card({img, rating, reviews, country, title, price}){
-    return (
-        <div className="card">
-            <img src={img} alt="car-image" className="card-image"/>
-            <div className="card-details">
-                <img src="./src/assets/star.png" alt="star" className="card-star" />
-                <span>{rating}</span>
-                <span className="gray">({reviews})</span>
-                <span className="gray">-{country}</span>
-            </div>
-            <p>{title}</p>
-            <p><span className="bold">from ${price}</span>/person</p>
-        </div>
-    )
+export default function Card({
+  img,
+  rating,
+  reviews,
+  country,
+  title,
+  price,
+  openspots,
+}) {
+  let badgeText;
+  if (openspots == 0) {
+    badgeText = "SOLD-OUT";
+  } else if (country === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+      <div className="card">
+        {badgeText && <div className="sold-badge">{badgeText}</div>}
+      <img src={img} alt="car-image" className="card-image" />
+      <div className="card-details">
+        <img src="./src/assets/star.png" alt="star" className="card-star" />
+        <span>{rating}</span>
+        <span className="gray">({reviews})</span>
+        <span className="gray">-{country}</span>
+      </div>
+      <p>{title}</p>
+      <p>
+        <span className="bold">from ${price}</span>/person
+      </p>
+    </div>
+  );
 }
